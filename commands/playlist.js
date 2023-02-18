be526559c6f0407e0b085a20a46f94a4829c7f2e
giovanna <giovannaferreiras20@gmail.com>
2023-02-18 17:05:16 -0300

    Bot discord

---
+++ commands/playlist.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("playlist")
+        .setDescription("Ouça a melhor playlist de estudos"),
+
+    async execute(interaction) {
+        await interaction.reply("https://open.spotify.com/playlist/5C6ErsAsdzI3uvozWxi9EO?si=1ee6df945b874dcc")
+    }
+}
